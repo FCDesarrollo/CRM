ecfe1c9b5cabed3dd513a4c8ffdab87fb2938fb1
Alejandro Paez <46059841+AlejadroPaez@users.noreply.github.com>
2019-02-20 17:49:04 -0700

    Prueba comandos cmd

--- modal/ajax/archivos_ajax.php
+++ modal/ajax/archivos_ajax.php
@@ -3,6 +3,11 @@
     $certificado = $_FILES["archivoCer"];    
     $llave = $_FILES["archivoKey"]; 
 
+    $fileCert = $certificado["tmp_name"];
+    $remote_fileCer = $nom.'/'.$certificado["name"];
+    $fileKey = $llave["tmp_name"];
+    $remote_fileKey = $nom.'/'.$llave["name"];
+
     /*foreach ($certificado as $key => $value) {
         echo $key." ".$value."   ->";
     }*/
@@ -16,38 +21,97 @@
     $ftp_user_pass = "4u1B6nyy3W";
     $login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
 
-    // try to create the directory $dir
-    if (ftp_mkdir($conn_id, $nom)) {
-        echo "successfully created $nom\n";        
-//        echo " successfully created $nom\n";
-    } else {
-        echo "There was a problem while creating $nom\n";
-    }
-
-    $file = $certificado["tmp_name"];
-    $remote_file = $nom.'/'.$certificado["name"];
-
-    // upload a file
-    if($certificado["type"] == "application/x-x509-ca-cert"){
-        if (ftp_put($conn_id, $remote_file, $file, FTP_ASCII)) {
-            echo "successfully uploaded $file\n";
+    if (file_exists($nom)) {            
+        echo "El directorio ya existe";
+    }else{
+        // try to create the directory $dir
+        if (ftp_mkdir($conn_id, $nom)) {
+            echo "Carpeta $nom creada con exito\n";        
         } else {
-            echo "There was a problem while uploading $file\n";
+            echo "Ocurrio un problema al crear a carpeta $nom\n";
+        }   
+        // upload a file
+        if($certificado["type"] == "application/x-x509-ca-cert" && $llave["type"] == "application/octet-stream"){
+            if (ftp_put($conn_id, $remote_fileCer, $fileCert, FTP_ASCII)) {
+                echo "Archivo $fileCert guardado con extio\n";
+            } else {
+                echo "Ocurrio un problema con el archivo $fileCert\n";
+            }
+            if (ftp_put($conn_id, $remote_fileKey, $fileKey, FTP_ASCII)) {
+                echo "Archivo $fileKey guardado con extio\n";
+            } else {
+                echo "Ocurrio un problema con el archivo $fileKey\n";
+            }
+        }else {
+            if (ftp_rmdir($conn_id, $nom)) {
+                echo "Se ha eliminado correctamente el directorio $nom\n";
+            } else {
+                echo "Hubo un problema al eliminar el directorio $nom\n";
+            }
+            echo "Los archivos no corresponden a     tipo certificado o llave";
         }
-    }
 
-    $file = $llave["tmp_name"];
-    $remote_file = $nom.'/'.$llave["name"];
+        $command = 'cd..';
 
-
-    if($llave["type"] == "application/octet-stream"){
-        if (ftp_put($conn_id, $remote_file, $file, FTP_ASCII)) {
-            echo "successfully uploaded $file\n";
+        if (ftp_exec($conn_id, $command)) {
+            echo "$command executed successfully\n";
         } else {
-            echo "There was a problem while uploading $file\n";
+            echo "could not execute $command\n";
         }
+
+        $salida = shell_exec('mkdir dir1');
+        echo $salida;
+
+        //$password = "FRANCO144B";
+        //$salida  = scandir('/files');
+        //$salida = shell_exec('openssl.exe pkcs8 -inform DER -in '.$remote_fileKey.' -out '.$remote_fileKey.'.pem -passin pass:'.$password.' 2>&1');
+        //$salida = shell_exec('mkdir dir1');
+        /*$command = "mkdir dir1";
+        $resultado = ftp_exec($conn_id, $command);
+        echo $resultado;
+*/
+        $password = "FRANCO144B"; //$_POST["txtContra"];
+        //$nombreKey = $fileKey; //"C:/xampp/htdocs/script/00001000000407734879.cer"; //$_FILES["archivoCer"];   
+        
+        $command = 'openssl.exe pkcs8 -inform DER -in "'.$remote_fileKey.'" -out "'.$remote_fileKey.'.pem" -passin pass:"'.$password.'" 2>&1';
+
+        if (ftp_exec($conn_id, $command)) {
+            echo "$command executed successfully\n";
+        } else {
+            echo "could not execute $command\n";
+        }
+
+        $salida = shell_exec('mkdir dir1');
+        echo $salida; /*
+        $command = "ls";
+        $command = 'openssl pkcs8 -inform DER -in "'.$remote_fileKey.'" -out "'.$remote_fileKey.'.pem" -passin pass:"'.$password.'" 2>&1';
+        $resultado = ftp_exec($conn_id, $command, strlen("SITE EXEC"));
+        echo $resultado;
+        /*if (ftp_exec($conn_id, $command)) {
+            echo "$command executed successfully\n";
+        } else {
+            echo "could not execute $command\n";
+        }
+        /*
+        //Elimina archivos y carpeta
+        if (ftp_delete($conn_id, $remote_fileCer)) {
+            echo "$remote_fileCer deleted successful\n";
+        } else {
+            echo "could not delete $remote_fileCer\n";
+        }
+
+        if (ftp_delete($conn_id, $remote_fileKey)) {
+            echo "$remote_fileCer deleted successful\n";
+        } else {
+            echo "could not delete $remote_fileCer\n";
+        }
+        if (ftp_rmdir($conn_id, $nom)) {
+            echo "Se ha eliminado correctamente el directorio $nom\n";
+        } else {
+            echo "Hubo un problema al eliminar el directorio $nom\n";
+        }
+        */
     }
-    
 
     // close the connection
     ftp_close($conn_id);
--- modal/js/archivos.js
+++ modal/js/archivos.js
@@ -9,8 +9,26 @@ function subirArchivos() {
         beforesend: function(){
 
         },
-        success: function(response){            
-            alert(response);       
+        success: function(request){            
+            console.log(request);       
+            /*$.get(ws + "BDDisponible", function(data){
+                var resultado = JSON.parse(data).basedatos;             
+                if (resultado.length > 0){
+                    var id = resultado[0].id;                
+                    var rfc = document.getElementById("txtRFC").value;
+                    var nombre = resultado[0].nombre;   
+                    $("#txtempresaBD").val(nombre);
+                    $.post(ws + "AsignaBD",  { id: id, rfc: rfc }, function(data){    
+                        if(data>0){  
+                            ResgistraEmpresa();
+                        }else{
+                            alert("Ocurrio un problema 1");
+                        }
+                    });	
+                } else {
+                    alert("Ocurrio un problema 2");
+                }                  
+            });*/
         }, 
         error: function (response) {
             alert("Ha ocurrido un error al subir los archivos");
@@ -18,3 +36,28 @@ function subirArchivos() {
 
     });
 }
+function ResgistraEmpresa()
+    {                    
+        var status = "1";
+        var fechaReg = new Date();              
+            $("#txtIdEmpresa").val(IDEMPRESA);
+            $("#txtStatus").val(status);
+            $("#txtfecharegistro").val(fechaReg.getFullYear() + "/" + (fechaReg.getMonth() + 1) + "/" + fechaReg.getDate());    
+
+            $.post(ws + "GuardarEmpresa", $("#FormGuardarEmpresa").serialize(), function(data){
+                if(data>0){       
+                    $.post(ws + "CrearTablasEmpresa", $("#FormGuardarEmpresa").serialize(), function(result){
+                        if(result>0){                                                            
+                                $('#NuevaEmpresa').modal('hide');
+                                document.getElementById("FormGuardarEmpresa").reset();
+                            }else{
+                                alert("Ocurrio un error al crear tablas de la empresa");
+                            }
+                    });      
+                }else
+                {
+                    alert("Ocurrio un error al guardar el empleado 3");
+                }
+            });
+          
+    }
