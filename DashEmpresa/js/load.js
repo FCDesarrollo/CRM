4b343b2ac1e6f7231c4b5a415e2da60dbc806356
Enrique Aramburo <enrique_shady@hotmail.com>
2019-04-04 17:02:36 -0700

    AbrePDFs

--- DashEmpresa/js/load.js
+++ DashEmpresa/js/load.js
@@ -1,3 +1,36 @@
+//Constantes para los modulos
+const ModContabilidad = 1;
+const ModBandejaEntrada = 2;
+//Constantes para los menus
+const MenuContabilidad = 1;
+const MenuProcesoFiscal = 2;
+const MenuFinanzas = 3;
+const MenuCompras = 4;
+const MenuAlmacenDigital = 5;
+const MenuRecepcionLotes = 6;
+//Constantes para los submenus
+const SubEstadosFinancieros = 1;
+const SubContabilidadElectronica = 2;
+const SubExpedientesAdmin = 3;
+const SubExpedientesContables = 4;
+const SubPagosProvicionales = 5;
+const SubPagosMensuales = 6;
+const SubDeclaracionesAnuales = 7;
+const SubExpedientesFiscales = 8;
+const SubIndicadoresFinancieros = 9;
+const SubAsesorFlujoEfectivo = 10;
+const SubAnalisisProyecto = 11;
+const SubRequerimientos = 12;
+const SubAutorizaciones = 13;
+const SubRecepcionCompras = 14;
+const SubNotificacionesAutoridades = 15;
+const SubExpedientesDigitales = 16;
+const SubProcesoProduccion = 17;
+const SubProcesoCompras = 18;
+const SubProcesoVenta = 19;
+
+
+
 function CargaDatosEmpresa(idusuario, idempresalog){   
         
 
@@ -17,39 +50,119 @@ function CargaDatosEmpresa(idusuario, idempresalog){
 }
 
 
-function CargaContenido(idsubmenu, RFCEmpresa){
+function CargaContenido(idmodulo, idmenu, idsubmenu, RFCEmpresa){
 
     $('#loading').removeClass('d-none');
 
+
     $('#divdinamico').load('../submenus/contenidos.php');
     
-    
     var ruta = "";
+    var modulo = "";
+    var menu = "";
     var submenu = "";
-    switch(idsubmenu){
-        case 1:
-            submenu = "EEFF";
+
+    switch(idmodulo){
+        case ModContabilidad:
+            modulo = "Contabilidad";
+            switch(idmenu){
+                case MenuContabilidad:
+                    menu = "Contabilidad";
+                break;
+                case MenuProcesoFiscal:
+                    menu = "ProcesoFiscal";
+                break;
+                case MenuFinanzas:
+                    menu = "Finanzas";
+                break;
+            }
+        break;
+        case ModBandejaEntrada:
+            modulo = "Bandeja";
+            switch(idmenu){
+                case MenuCompras:
+                    menu = "Compras";
+                break;
+                case MenuProcesoFiscal:
+                    menu = "AlmacenDigital";
+                break;
+                case MenuRecepcionLotes:
+                    menu = "RecepcionporLotes";
+                break;
+            }    
             break;
-        case 2:
-            //submenu = "ContElec";
-            submenu = "Contabilidad";
-            break;
-        case 3:
-            submenu = "ExpAdmin";
-            break;
-        case 4:
-            submenu = "ExpCont";
-            break;                                                    
     }
 
 
-    ruta = "../../../nextclouddata/admindublock/files/PruebaSincro/"+RFCEmpresa+"/"+submenu+"/";
+    switch(idsubmenu){
+        case SubEstadosFinancieros:
+            submenu = "EstadosFinancieros";
+            break;
+        case SubContabilidadElectronica:
+            submenu = "ContabilidadElectronica";
+            break;
+        case SubExpedientesAdmin:
+            submenu = "ExpedientesAdministrativos";
+            break;
+        case SubExpedientesContables:
+            submenu = "ExpedientesContables";
+            break;
+        case SubPagosProvicionales:
+            submenu = "PagosProvisionales";
+            break;
+        case SubPagosMensuales:
+            submenu = "PagosMensuales";
+            break;
+        case SubDeclaracionesAnuales:
+            submenu = "DeclaracionesAnuales";
+            break;
+        case SubExpedientesFiscales:
+            submenu = "ExpedientesFiscales";
+            break;
+        case SubIndicadoresFinancieros:
+            submenu = "IndicadoresFinancieros";
+            break;
+        case SubAsesorFlujoEfectivo :
+            submenu = "AsesordeFlujosdeEfectivo";
+            break;
+        case SubAnalisisProyecto :
+            submenu = "AnalisisdeProyectos";
+            break;
+        case SubRequerimientos :
+            submenu = "Requerimientos";
+            break;
+        case SubAutorizaciones :
+            submenu = "Autorizaciones";
+            break;
+        case SubRecepcionCompras :
+            submenu = "Recepcióndecompras";
+            break;
+        case SubNotificacionesAutoridades :
+            submenu = "NotificacionesdeAutoridades";
+            break;
+        case SubExpedientesDigitales :
+            submenu = "ExpedientesDigitales";
+            break;  
+        case SubProcesoProduccion :
+            submenu = "ProcesodeProducción";
+            break;
+        case SubProcesoCompras :
+            submenu = "ProcesodeCompras";
+            break;
+        case SubProcesoVenta :
+            submenu = "ProcesodeVentas";
+            break;
+    }
+
+
+    ruta = "../nextclouddata/admindublock/files/PruebaSincro/"+RFCEmpresa+"/"+modulo+"/"+menu+"/"+submenu+"/";
+    //ruta = "../../../nextclouddata/admindublock/files/PruebaSincro/"+RFCEmpresa+"/"+modulo+"/"+menu+"/"+submenu+"/";
     //ruta = "../archivospdf/"+RFCEmpresa+"/"+submenu+"/";
 
     $.ajax({
         url: '../submenus/leer_carpeta.php',
         type: 'POST',        
-        data: {submenu: submenu, RFCEmpresa: RFCEmpresa},
+        data: {modulo: modulo, menu: menu, submenu: submenu, RFCEmpresa: RFCEmpresa},
         success:function(data){
             var archivo = "";
             var nombrearchivo = "";
@@ -67,7 +180,7 @@ function CargaContenido(idsubmenu, RFCEmpresa){
                           </label> \
                         </td> \
                         <td> \
-                          <a href='"+ruta+array2[x].nombre+"' target='_blank'><i class='fa fa-file-pdf-o tx-22 tx-danger lh-0 valign-middle'></i> \
+                          <a href='#' onclick=AbrirPDF('"+ruta+"','"+array2[x].nombre+"')><i class='fa fa-file-pdf-o tx-22 tx-danger lh-0 valign-middle'></i> \
                           <span class='pd-l-5'>"+array2[x].nombre+"</span></a> \
                         </td> \
                         <td class='hidden-xs-down'>"+array2[x].fecha+"</td> \
@@ -75,8 +188,8 @@ function CargaContenido(idsubmenu, RFCEmpresa){
                           <a href='#' data-toggle='dropdown' class='btn pd-y-3 tx-gray-500 hover-info'><i class='icon ion-more'></i></a> \
                           <div class='dropdown-menu dropdown-menu-right pd-10'> \
                             <nav class='nav nav-style-1 flex-column'> \
-                              <a href='"+ruta+array2[x].nombre+"' target='_blank' class='nav-link'>Abrir</a> \
-                              <a href='"+ruta+array2[x].nombre+"' download='"+archivo+"' id='download_"+nombrearchivo+"' class='nav-link'>Descargar</a> \
+                              <a href='#' onclick='' class='nav-link'>Abrir</a> \
+                              <a href='#' onclick='' class='nav-link'>Descargar</a> \
                             </nav> \
                           </div> \
                         </td> \
@@ -105,11 +218,21 @@ function CargaContenido(idsubmenu, RFCEmpresa){
             
          }
                 
+    }); 
+
+
+}
+
+function AbrirPDF(RutaArchivo, Archivo){
+
+    //console.log(RutaArchivo);
+    //console.log(Archivo);
+    $.ajax({
+         url: '../../../cargapdf.php',
+         type: 'POST',        
+         data: {ruta: RutaArchivo, ArchivoPDF: Archivo},
     });
-        
-        
-
-
+    
 }
 
 function CompartirArchivos(){
@@ -117,6 +240,7 @@ function CompartirArchivos(){
 }
 
 function DescargarArchivos(RFCEmpresa){
+    $('#ruta').removeClass('d-none');
     var url = "../../../nextclouddata/admindublock/files/PruebaSincro/"+RFCEmpresa+"/";
     //var url = "../archivospdf/"+RFCEmpresa+"/";
 
@@ -127,11 +251,16 @@ function DescargarArchivos(RFCEmpresa){
         celdas = $(filas[i]).find("input"); //devolverá las celdas de una fila
         var id = celdas[0].id;
         var name = celdas[0].name;
-        url2 = url+name+"/"+id+".pdf";  
+        //url2 = url+name+"/"+id+".pdf";  
         
         if($('#'+id).prop('checked')){
+            /*
             var doc = "download_"+id;
-            document.getElementById(doc).click();
+            document.getElementById(doc).click(); */
+
+
+            var rutanueva = showModalDialog("folderDialog.html","","width:400px;height:400px;resizeable:yes;");
+
         }
     }
 }
