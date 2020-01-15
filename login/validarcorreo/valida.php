a64a8baa8f83e3f8cf74d7b8ff3dcd68e8108f8b
Enrique Aramburo <enrique_shady@hotmail.com>
2020-01-14 18:01:29 -0700

    Update valida.php

--- login/validarcorreo/valida.php
+++ login/validarcorreo/valida.php
@@ -27,8 +27,8 @@ $url_validacorreo = 'http://crm.dublock.com/login/';
 
 $mail = new PHPMailer;
 $mail->isSMTP();                                      // Activamos SMTP para mailer
-$mail->SMTPDebug = 2;
-$mail->Debugoutput = 'html';
+//$mail->SMTPDebug = 2;
+//$mail->Debugoutput = 'html';
 $mail->Host = 'mail.dublock.com';                       // Especificamos el host del servidor SMTP
 $mail->SMTPAuth = true;                               // Activamos la autenticacion
 //$mail->Username = 'miconsultormx@gmail.com';       // Correo SMTP
